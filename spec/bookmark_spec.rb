9e1c8b1ee1e32d8b2ffa261e377cb0a52798e9bc
Cynthia Fu <yinnycfu@gmail.com>
2021-06-16 16:55:38 +0100

    Step 9: setting up a test environment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,11 +5,18 @@ require 'bookmark'
 describe Bookmark do
   context '.all' do
     it 'displays all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.bbc.co.uk');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include('http://www.makersacademy.com')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.bbc.co.uk')
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
+require 'pg'
 
 feature 'Viewing bookmarks' do
   scenario 'seeing list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(4, 'http://www.bbc.co.uk');")
+
     visit '/bookmarks'
     expect(page).to have_content 'http://www.makersacademy.com'
     expect(page).to have_content 'http://www.destroyallsoftware.com'
     expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_content 'http://www.bbc.co.uk'
   end
 end
